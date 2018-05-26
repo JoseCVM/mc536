4f8953ba64e1838e7358460acf4542f985b250fd
marinagui <marinagui10@hotmail.com>
2018-05-26 10:19:24 -0300

    fix populate_partidas script

--- back/insert_script.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO pessoa (id_pessoa, tipo_documento, documento, nome) VALUES (1, 'CPF', '3123123', 'joao')
-INSERT INTO cidade VALUES(1, 'Moscow', 'Centro')
-INSERT INTO estadio VALUES(1, 1, 'estadio1', 3000, 'endereco_estadio1', 'estadio 1 inserido', 'foto_estadio1.jpg')
-INSERT INTO equipe_de_gravacao VALUES(1, 'equipe_gravacao 1')
-INSERT INTO partida VALUES (1, '2018-07-13', '16:00:00', 'Primeira fase', 1, 1, 1)
-INSERT INTO lance VALUES (1, 1, 1531497600, 'Gol', 'gol x blah')
-
-INSERT INTO selecao VALUES('BRA', 'Brasil', 'Seleção do Brasil', 1, 'CBF', 'bandeira_brasil.jpg')
-INSERT INTO jogador VALUES(1, 'BRA', 'neymar', 'jogador TOP', 'Artilheiro', 'Flamengo', 10)
